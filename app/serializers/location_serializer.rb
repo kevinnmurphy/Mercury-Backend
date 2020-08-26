e2d57d7522508a6b9bbc6fdde559b67affdb4847
qu1xs <kevinnealmurphy@gmail.com>
2020-08-25 21:37:29 -0400

    add create stamp

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,5 +1,5 @@
 class LocationSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :lat, :lon, :trip_id
+  attributes :id, :name, :lat, :lon, :updated_at
   belongs_to :trip
 end
