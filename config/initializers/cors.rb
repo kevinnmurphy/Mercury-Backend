df86e11a8fa591830f3ea02f34c93f6f8fab8626
KevinnMurphy <kevinnealmurphy@gmail.com>
2020-09-14 16:00:32 -0400

    remove the slash

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins '*'
-    origins 'https://quizzical-kalam-176698.netlify.app/'
+    origins 'https://quizzical-kalam-176698.netlify.app'
     # origins 'http://localhost:3000/'
 
     resource '*',
