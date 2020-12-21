1c68cbae6986174f15f0ebbe2c07b3200e2bbd93
KevinnMurphy <kevinnealmurphy@gmail.com>
2020-12-21 10:20:26 -0500

    reset cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins '*'
-    origins 'https://project-mercurial.netlify.app/*'
-    origins 'http://localhost:3000/'
+    origins '*'
 
     resource '*',
       headers: :any,
